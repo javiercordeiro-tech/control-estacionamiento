f627a2b9e19f4e71c5695c9a7f023b68886d2ca5
javiercordeiro-tech <javier.cordeiro@envases.mx>
2025-11-14 11:11:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURACIÓN DE LA BASE DE DATOS (GOOGLE APPS SCRIPT URL)
 // ==========================================================
 
-// ¡Tu nueva URL única del servidor de Apps Script ya está integrada aquí!
+// ¡Tu URL única del servidor de Apps Script ya está integrada aquí!
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw6QHRAdAQTWZnDK7Awh_iKCzLHD_yuIAtC0T3shNOUgxG9eFG7g3hfPifSj1y4p2Cy/exec';
 
 
@@ -43,7 +43,7 @@ async function cargarEstadoEstacionamiento() {
 
     } catch (error) {
         console.error("Error al cargar el estado del estacionamiento:", error);
-        alert("ERROR: No se pudo conectar a la base de datos. Revisa la URL de la App Web o los permisos.");
+        alert("ERROR: No se pudo conectar a la base de datos. Asegúrate que el acceso esté en 'Cualquier persona' y la pestaña en 'Lugares'.");
     }
 }
 
